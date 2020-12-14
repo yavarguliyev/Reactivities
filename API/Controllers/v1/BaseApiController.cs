f8d67d33dfe91e7b30cd63dce572eb2155370856
yavarguliyev <yavarrg@code.edu.az>
2020-12-14 20:52:00 +0400

    base api controller added

---
+++ API/Controllers/v1/BaseApiController.cs
@@ -0,0 +1,17 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Controllers.v1
+{
+  [ApiController]
+  [ApiVersion("1.0")]
+  [Route("api/v1/[controller]")]
+  public class BaseApiController : ControllerBase
+  {
+    private IMediator _mediator;
+
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+        .GetService<IMediator>();
+  }
+}
