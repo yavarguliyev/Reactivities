930b1a85ab8ad22b31cee869431532c8699a8da4
yavarguliyev <yavarrg@code.edu.az>
2020-12-15 00:22:22 +0400

    client-app folder structure added

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+const App = () => {
+  return (
+    <></>
+  );
+};
+
+export default App;
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActivityDashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ActivityDashboard
