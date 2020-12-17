9273b83de7a128bd60baad9081b0e5aa535d2831
yavarguliyev <yavarrg@code.edu.az>
2020-12-17 22:27:45 +0400

    startup swagger auth modified

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,6 +22,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using System.Collections.Generic;
 
 namespace API
 {
@@ -91,8 +92,8 @@ namespace API
               });
               options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
               {
-                Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
-                Name = "Authorization: Bearer",
+                Description = "JWT Authorization header using the Bearer scheme (Example: Authorization: 'Bearer {token}')",
+                Name = "Authorization",
                 In = ParameterLocation.Header,
                 Type = SecuritySchemeType.ApiKey,
                 Scheme = "Bearer"
@@ -174,6 +175,7 @@ namespace API
       app.UseSwaggerUI(s =>
       {
         s.RoutePrefix = "";
+        s.DocumentTitle = "Swagger Documentation";
         s.SwaggerEndpoint("/swagger/v1/swagger.json", "api/v1");
       });
     }
